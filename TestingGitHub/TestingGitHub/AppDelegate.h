e3db67f3da0657f9f00ea72590b6fc48054c8d3c
Gene Backlin <genebacklin@Genes-Air.att.net>
2014-10-13 11:26:06 -0500

    Initial

---
+++ TestingGitHub/TestingGitHub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestingGitHub
+//
+//  Created by Gene Backlin on 10/13/14.
+//  Copyright (c) 2014 Gene Backlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestingGitHub/TestingGitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestingGitHub
+//
+//  Created by Gene Backlin on 10/13/14.
+//  Copyright (c) 2014 Gene Backlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
